449e2a9816392766e1f34f287f256ce863509b73
abdel-17 <abdelrahmanb015@gmail.com>
2023-12-23 08:21:51 +0200

    feat: add database

---
+++ lib/data/database.dart
@@ -0,0 +1,80 @@
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+import './note.dart';
+
+class _NotebookDatabase {
+  static const _name = "notebook.db";
+  static const _version = 1;
+
+  static Future<Database> init() async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, _name);
+    return await openDatabase(path, version: _version, onCreate: _onCreate);
+  }
+
+  static _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE ${Note.tableName} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT,
+        created_at TEXT
+      )
+  ''');
+  }
+}
+
+class Dao {
+  final Database _db;
+
+  Dao._(this._db);
+
+  // Maintain a single instance of the dao.
+  static Dao? _instance;
+
+  static Future<Dao> get instance async {
+    final instance = _instance;
+    if (instance != null) {
+      return instance;
+    }
+
+    final db = await _NotebookDatabase.init();
+    final dao = Dao._(db);
+    _instance = dao;
+    return dao;
+  }
+
+  Future<List<Note>> getNotes() async {
+    final result = await _db.query(
+      Note.tableName,
+      columns: ['id', 'title', 'content', 'created_at'],
+      orderBy: 'created_at DESC',
+    );
+    return result.map(Note.fromMap).toList();
+  }
+
+  Future<Note> getNote({required int id}) async {
+    final result = await _db.query(
+      Note.tableName,
+      columns: ['id', 'title', 'content', 'created_at'],
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    return Note.fromMap(result.first);
+  }
+
+  Future<void> insertNote(Note note) async {
+    await _db.insert(Note.tableName, note.toMap());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await _db.update(
+      Note.tableName,
+      note.toMap(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+}
---
+++ lib/data/note.dart
@@ -0,0 +1,37 @@
+class Note {
+  static const tableName = 'notes';
+
+  final int id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+
+  const Note({
+    this.id = 0,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> result = {
+      'title': title,
+      'content': content,
+      'created_at': createdAt.toIso8601String(),
+    };
+    if (id != 0) {
+      // 0 is treated as unset.
+      result['id'] = id;
+    }
+    return result;
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      content: map['content'],
+      createdAt: DateTime.parse(map['created_at']),
+    );
+  }
+}
