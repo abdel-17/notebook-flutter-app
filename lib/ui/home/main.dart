ce1daea437b1b2743a7106868bf7079e17d3053e
abdel-17 <abdelrahmanb015@gmail.com>
2023-12-24 07:35:29 +0200

    feat: modify note list item text style

--- lib/ui/home/main.dart
+++ lib/ui/home/main.dart
@@ -65,8 +65,10 @@ class _NoteListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(note.title),
-      subtitle: Text(note.content.firstLine, overflow: TextOverflow.ellipsis),
+      title: Text(note.title, style: Theme.of(context).textTheme.titleMedium),
+      subtitle: Text(note.content.firstLine,
+          style: Theme.of(context).textTheme.bodyMedium,
+          overflow: TextOverflow.ellipsis),
     );
   }
 }
